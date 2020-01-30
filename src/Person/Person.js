888c1e2082707a0e69ae1ff63a3e8c76f79ed5b6
Dariusz Franczak <darius32@op.pl>
2020-01-30 23:31:05 +0000

    completed

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,7 +4,7 @@ const person = (props) => {
   return (
     <div>
       <p>My name is {props.name} and I'm {props.age}</p>
-      <input type="text" onChange={props.changed} />
+      <input type="text" value={props.name} onChange={props.changed} />
     </div>
   )
 }
